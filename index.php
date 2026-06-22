1ba41042dbb173c014dc1fdf5a010fbe34b23272
Francisco Blanco <blancofrancisco34@gmail.com>
2026-06-22 18:25:18 -0500

    Release 1.0.20

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Martfury Custom
 Plugin URI: https://github.com/franciscoblancojn/martfury-custom
 Description: Este plugin agrega customizaciones para el tema Martfury.
-Version: 1.0.19
+Version: 1.0.20
 Author: franciscoblancojn
 Author URI: https://franciscoblanco.vercel.app/
 License: GPL2+
